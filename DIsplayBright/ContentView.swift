1b805aed0ef5a9afb62a44d72a6aa782683454dc
J.R. Phillips <43276115+JayAreP@users.noreply.github.com>
2026-04-09 07:58:51 -0700

    Initial Commit

---
+++ DIsplayBright/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DIsplayBright
+//
+//  Created by Josh Phillips on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DIsplayBright/DIsplayBrightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIsplayBrightApp.swift
+//  DIsplayBright
+//
+//  Created by Josh Phillips on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DIsplayBrightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
